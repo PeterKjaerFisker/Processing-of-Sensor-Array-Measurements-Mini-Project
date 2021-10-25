ae371da82b24fd80f9e1edb6001bb8ff32177fa5
Nicolai <nara@student.aau.dk>
2021-10-25 09:06:42 +0200

    draft: added a python file
    Signed-off-by: Nicolai <nara@student.aau.dk>

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 09:04:24 2021
+
+@author: Nicolai Almskou Rasmusen &
+         Victor Mølbach Nissen
+"""
+
+# %% Imports
+
+
+# %% Main
+
+if __name__ == '__main__':
+    print("Hello World")  # Prints "Hello World"
