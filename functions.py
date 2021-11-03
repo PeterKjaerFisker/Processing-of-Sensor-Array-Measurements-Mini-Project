4e4471a2c9d453e4b6f1091462b86f4a661032e6
Nicolai <nara@student.aau.dk>
2021-11-03 15:33:45 +0100

    rebase + removed redundant code
    Signed-off-by: Nicolai <nara@student.aau.dk>

--- functions.py
+++ functions.py
@@ -29,40 +29,6 @@ def delay_respons_vector(theta, tau, r, f, lambda_):
     return np.kron(b, a)
 
 
-def MUSIC(R, Res, dat, idx_tau, idx_array, M):
-    # Parameters
-    Tau = dat['tau']
-    f = dat['f'][idx_tau]
-    f0 = dat['f0']
-    r = dat['r'][:, idx_array.reshape(len(idx_array))]
-
-    lambda_ = 3e8/f0
-
-    Theta = np.linspace(0, 2*np.pi, Res[0])
-    Pm = np.zeros([Res[0], len(Tau)])
-
-    # ------ Step 3 - Form U ------
-    E, U = np.linalg.eig(R)
-    Un = U[:, M:]
-
-    # ------ Step 4 - Calculate Freq. estimate ------
-    # Do the caluclations
-    for i in range(len(Theta)):
-        print(i)
-        for j in range(len(Tau)):
-            # Calculate for the different steering matrix
-            As = delay_respons_vector(Theta[i], Tau[j], r, f, lambda_)
-
-            Ash = np.conjugate(As).T
-            Unh = np.conjugate(Un).T
-
-            Pm[i, j] = 1/np.abs(Ash@Un@Unh@As)
-
-            # print(f"step {i*len(Tau) + j + 1} out of {Res*len(Tau)}")
-
-    return Pm
-
-
 def estM(E, N, pn):
     p = np.arange(1, pn+1).reshape([1, pn])
 
@@ -167,6 +133,40 @@ def barlettRA(X, Res, dat, idx_tau, idx_array):
     return np.abs(DTFT_aoa@X@DTFTconj_delay)
 
 
+def MUSIC(R, Res, dat, idx_tau, idx_array, M, tau_search):
+    # Parameters
+    Tau = np.linspace(tau_search[0], tau_search[1], Res[1], endpoint=True)
+    f = dat['f'][idx_tau]
+    f0 = dat['f0']
+    r = dat['r'][:, idx_array.reshape(len(idx_array))]
+
+    lambda_ = 3e8/f0
+
+    Theta = np.linspace(0, 2*np.pi, Res[0])
+    Pm = np.zeros([Res[0], len(Tau)])
+
+    # ------ Step 3 - Form U ------
+    E, U = np.linalg.eig(R)
+    Un = U[:, M:]
+
+    # ------ Step 4 - Calculate Freq. estimate ------
+    # Do the caluclations
+    for i in range(len(Theta)):
+        print(i)
+        for j in range(len(Tau)):
+            # Calculate for the different steering matrix
+            As = delay_respons_vector(Theta[i], Tau[j], r, f, lambda_)
+
+            Ash = np.conjugate(As).T
+            Unh = np.conjugate(Un).T
+
+            Pm[j, i] = 1/np.abs(Ash@Un@Unh@As)
+
+            # print(f"step {i*len(Tau) + j + 1} out of {Res*len(Tau)}")
+
+    return Pm
+
+
 def barlett(R, Res, dat, idx_tau, idx_array, tau_search):
     # Parameters
     Tau = np.linspace(tau_search[0], tau_search[1], Res[1], endpoint=True)
@@ -198,20 +198,21 @@ def barlett(R, Res, dat, idx_tau, idx_array, tau_search):
     return Pm
 
 
-def capon(R, Res, dat, idx_tau, idx_array):
+def capon(R, Res, dat, idx_tau, idx_array, tau_search):
     # Parameters
-    Tau = dat['tau']
+    Tau = np.linspace(tau_search[0], tau_search[1], Res[1], endpoint=True)
     f = dat['f'][idx_tau]
     f0 = dat['f0']
     r = dat['r'][:, idx_array.reshape(len(idx_array))]
 
     lambda_ = 3e8/f0
 
-    Theta = np.linspace(0, 2*np.pi, Res)
-    Pm = np.zeros([Res, len(Tau)])
+    Theta = np.linspace(0, 2*np.pi, Res[0])
+    Pm = np.zeros([Res[0], len(Tau)])
 
     # Do the caluclations
     for i in range(len(Theta)):
+        print(i)
         for j in range(len(Tau)):
             # Calculate for the different steering matrix
             As = delay_respons_vector(Theta[i], Tau[j], r, f, lambda_)
@@ -220,106 +221,6 @@ def capon(R, Res, dat, idx_tau, idx_array):
 
             Rinv = np.linalg.inv(R)
 
-            Pm[i, j] = 1/np.abs(Ash@Rinv@As)
-
-            print(f"step {i*len(Tau) + j + 1} out of {Res*len(Tau)}")
+            Pm[j, i] = 1/np.abs(Ash@Rinv@As)
 
     return Pm
-
-# ---- Old ----
-def steering_matrix_1d(L, theta, d, lambda_):
-    return np.exp(1j*2*np.pi*np.arange(0, L).reshape(L, 1) *
-                  (d/lambda_)*np.cos(theta))
-
-
-def steering_matrix_2d(L2d, AoA, d, lambda_):
-    theta, phi = AoA
-    Lx, Ly = L2d
-    dx, dy = d
-
-    eta_x = (dx/lambda_)*np.sin(theta)*np.cos(phi).reshape(1, np.size(theta))
-    eta_y = (dy/lambda_)*np.sin(theta)*np.sin(phi).reshape(1, np.size(theta))
-
-    A = []
-
-    for i in range(Lx):
-        A.append(np.exp(1j*2*np.pi*(
-            np.ones([Lx, 1])*i*eta_x +
-            np.arange(0, Ly).reshape(Ly, 1)*eta_y)))
-
-    return np.concatenate(A)
-
-
-def get_peaks(Pm, Res, M):
-    Theta = np.linspace(0, np.pi, Res)*(180/np.pi)
-
-    peaks, _ = find_peaks(Pm.reshape(Res))
-    tmp = Pm[peaks]
-    tmp_sort = np.argsort(tmp, axis=0)
-
-    return Theta[peaks[tmp_sort[-M:]]].reshape([np.min([M, len(tmp_sort)])])
-
-
-def DMLE(theta, x, L, M):
-    R = np.cov(x, bias=True)
-
-    A = steering_matrix_1d(L, theta)
-    AT = np.linalg.pinv(A)
-    A_ort = np.eye(L)-A@AT
-
-    return (1/(L-M))*np.trace(A_ort@R)
-
-
-def ESPRIT(R, Res, M, L2d):
-
-    E, U = np.linalg.eig(R)
-    Us = U[:, :M]
-
-    # Calculate selections matrices
-    J1 = np.eye(L2d[0])[:-1, :]
-    J2 = np.eye(L2d[0])[1:, :]
-
-    Ju1 = np.kron(np.eye(L2d[1]), J1)
-    Ju2 = np.kron(np.eye(L2d[1]), J2)
-
-    Jv1 = np.kron(J1, np.eye(L2d[1]))
-    Jv2 = np.kron(J2, np.eye(L2d[1]))
-
-    # ------ Step 4 - Calculate C ------
-    # Calculate subarrays
-    phi_u = np.linalg.pinv(Ju1@Us)@Ju2@Us
-    phi_v = np.linalg.pinv(Jv1@Us)@Jv2@Us
-
-    # ------ Step 5 - Calculate Eigenvalues ------
-    Eu, Uu = np.linalg.eig(phi_u)
-    Ev, Uv = np.linalg.eig(phi_v)
-
-    u = (1/np.pi)*np.angle(Eu)
-    v = (1/np.pi)*np.angle(Ev)
-
-    theta = np.angle(u+1j*v)
-    phi = np.arcsin(np.abs(u+1j*v))
-
-    return theta, phi
-
-
-def test(X, Res, dat, idx_tau, idx_array):
-
-    Tau = dat['tau']
-    f = dat['f'][idx_tau]
-    # Tau = np.linspace(0, 1, 300).reshape([300, 1])
-    # f = np.arange(0, 101).reshape([101, 1])
-    f0 = dat['f0']
-    r = dat['r'][:, idx_array.reshape(len(idx_array))]
-
-    lambda_ = 3e8/f0
-
-    Theta = np.linspace(0, 2*np.pi, Res)
-
-    a = (np.exp(-1j*2*np.pi*(1/lambda_) *
-         np.array([np.cos(Theta), np.sin(Theta)]).T@r))
-
-    b = (np.exp(-1j*2*np.pi*f@Tau.T))
-
-    return a@X@np.conjugate(b)
-
